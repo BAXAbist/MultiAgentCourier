f717b64abca776a2104d1acc621bff33920ad511
BAXAbist <tolik.daf.777@gmail.com>
2022-12-30 17:01:13 +0300

    first commit

---
+++ src/Rover.java
@@ -0,0 +1,9 @@
+import jade.core.Agent;
+import jade.core.AID;
+import jade.core.behaviours.CyclicBehaviour;
+
+public class Rover extends Agent{
+
+    private final AID operator = new AID("operator", AID.ISLOCALNAME);
+
+}
